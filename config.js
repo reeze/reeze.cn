8914a62f3e541f937067eb9df2c93e4fda8e6b02
Reeze Xia <reeze@php.net>
2014-10-13 17:31:13 +0800

    Add my test conf

--- config.js
+++ config.js
@@ -28,6 +28,43 @@ config = {
         }
     },
 
+    reeze : {
+        // The url to use when providing links to the site, E.g. in RSS and email.
+        // Change this to your Ghost blogs published URL.
+        url: 'http://localhost:2369',
+
+        // Example mail config
+        // Visit http://support.ghost.org/mail for instructions
+        // ```
+        //  mail: {
+        //      transport: 'SMTP',
+        //      options: {
+        //          service: 'Mailgun',
+        //          auth: {
+        //              user: '', // mailgun username
+        //              pass: ''  // mailgun password
+        //          }
+        //      }
+        //  },
+        // ```
+
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-dev.db')
+            },
+            debug: false
+        },
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: 'localhost',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: '2369'
+        },
+        paths: {
+            contentPath: path.join(__dirname, '/content/')
+        }
+    },
     // ### Development **(default)**
     production: {
         // The url to use when providing links to the site, E.g. in RSS and email.
